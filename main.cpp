04ffe579fb136db5fbb53ea652e8d6fb370849dc
dennivision <93346040+dennivision@users.noreply.github.com>
2021-11-12 18:51:56 +0000

    converted plain text UDT's to code

--- main.cpp
+++ main.cpp
@@ -200,6 +200,44 @@ Thing 1) Aquarium Tank
     3) adjust pH
  */
 
+ struct AquariumTank
+ {
+    //1) the capacity of water it holds (float)
+    float waterCapacity = 10.f;
+    //2) the pH level of the water (float)
+    float pHLevel = 7.f;
+    //3) the current water level (float)
+    float currentWaterLevel = 0.7f;
+    //4) the amount of water that evaporates each day (double)
+    double evaporationRate = 0.0000005;
+    //5) the number of fish living in it (int)
+    int fishLivingIn = 0;
+
+    struct Fish
+    {
+      std::string Name = "new fish";
+      std::string Breed = "Goldfish";
+      float maxSalinity = 0.5f;
+      float age = 0.f;
+      float maxAge = 3.f;
+
+      //
+      bool canLiveInThisWater(float waterPH, float waterSalinity);
+      void ageFish(float amountOfTime);
+      bool isStillAlive(float waterPH, float waterSalinity)
+      {
+          return (canLiveInThisWater(waterPH, waterSalinity) && (age < maxAge));
+      }
+    };
+    // 1) add fish
+    void addFish(Fish fish, int Quantity);
+    // 2) add water
+    void addWater(float amountOfWater);
+    // 3) adjust pH
+    void adjustPH(float phAdjustment);
+    
+ };
+
 /*
 Thing 2) Museum
 5 properties:
@@ -207,13 +245,45 @@ Thing 2) Museum
     2) the number of employees (int)
     3) the total cost of utilities used each month (float)
     4) the monthly retail income amount (float)
-    5) the amount of government subsidy recieved each month (float)
+    5) the amount of government subsidy recieved each month (double)
 3 things it can do:
     1) charge visitor
     2) add or remove employees
     3) lobby politicians for more government funding
  */
+struct Museum
+{
+  // 1) the number of exhibits (int)
+  int exhibitCount = 5;
+  // 2) the number of employees (int)
+  int employeeCount = 0;
+  // 3) the total cost of utilities used each month (float)
+  float UtilityCost = 3500.f;
+  // 4) the monthly retail income amount (float)
+  float monthlyRetailIncome = 5000.f;
+  //5) the amount of government subsidy recieved each month (double)
+  double monthlyGovernmentSubsidy = 15000;
 
+  struct Visitor
+  {
+    std::string name = "visitor";
+    std::string nationality = "visitorsNationality";
+    int timesVisited = 0;
+    int age = 0;
+    float satisfaction = 0.5f;
+
+    float payAdmissionFee(float feeAmount);
+    void viewExhibit(int exhibitNum);
+    void payVendor(int vendorID, float amount);
+  };
+
+  // 1) charge visitor
+  void chargeVisitor(float amountToCharge, Visitor visitor) ;
+  // 2) add or remove employees
+  void addOrRemoveEmployees(int numberOfEmployees) { employeeCount += numberOfEmployees; }
+  // 3) lobby politicians for more government funding
+  void lobbyPoliticians(float bribeAmount);
+};
 /*
 Thing 3) Subwoofer Factory
 5 properties:
@@ -227,7 +297,26 @@ Thing 3) Subwoofer Factory
     2) purchase materials for subwoofers
     3) sell a subwoofer
  */
+struct SubwooferFactory
+{
+  // 1) amount of plywood in stock in boardfeet (float)
+  float plywoodInStock = 0.f;
+  // 2) number of subwoofer drivers in stock (int)
+  int subwwoferDriversInStock = 0;
+  // 3) number of employees (int)
+  int numberOfEmpoyees = 0;
+  // 4) average number of subwoofers completed each day (float)
+  float averageNumberOfSubwoofersCompletedEachDay = 0;
+  // 5) inventory of completed subwoofers (int)
+  int completedSubwoofers = 0;
 
+  // 1) assemble a subwoofer
+  bool assembleSubwoofer(float plywoodAmount, int driverAmount);
+  // 2) purchase plywood for subwoofers
+  void purchasePlywood(int sheets, float thickness);
+  // 3) sell a subwoofer
+  bool sellSubwoofer(float price);
+};
 /*
 Thing 4) Freight Train
 5 properties:
@@ -235,13 +324,35 @@ Thing 4) Freight Train
     2) the number of freight cars (int)
     3) the maximum cargo weight able to be pulled (float)
     4) the gross cargo weight in tons (float)
-    5) the trains speed in miles per hour (float)
+    5) the conductors name (std::string)
 3 things it can do:
     1) proceed to next stop
     2) pickup / dropoff cars
     3) blow airhorn
  */
-
+struct FreightTrain
+{
+  // 1) the number of locomotives (int)
+  int numberOfLocomotives = 1;
+  // 2) the number of freight cars (int)
+  int numberOfFreightCars = 1;
+  // 3) the maximum cargo weight able to be pulled (float)
+  float maxCargoWeight { numberOfLocomotives * 8000.f };
+  // 4) the gross cargo weight in tons (float)
+  float grossCargoWeight { numberOfFreightCars * 110.f };
+  // 5) the conductors name (std::string)
+  std::string conductorName = "conductor";
+  
+  // 1) proceed to next stop
+  void proceedToNextStop();
+  // 2) pickup / dropoff cars
+  void pickupOrDropoffCars(int numOfCars)
+  {
+    numberOfFreightCars += numOfCars;
+  }
+  // 3) blow airhorn
+  void blowAirHorn(float durationInSeconds);
+};
 /*
 Thing 5) Power Supply
 5 properties:
@@ -254,10 +365,29 @@ Thing 5) Power Supply
     1) The mains input voltage can be switched on / off
     2) the Main output voltage can be switched on independently (standby)
     3) The mains fuse can blow
- */
+*/
+struct PowerSupply
+{
+  // 1) Mains Input voltage (float)
+  float mainsInputVoltage = 110.f;
+  // 2) Main Output Voltage (Vcc) (float)
+  float mainOutputVoltage = 450.f;
+  // 3) Heater Output Voltage (float)
+  float heaterOutputVoltage = 6.3f;
+  // 4) Maximum Current (float)
+  float maximumCurrentInAmps = 1.f;
+  // 5) Fuse state (bool)
+  bool fuseState { true };
 
+  // 1) The mains input voltage can be switched on / off
+  void setPowerState(bool powerState);
+  // 2) the Main output voltage can be switched on independently (standby)
+  void setStandbyState(bool standbyState);
+  // 3) The mains fuse can blow
+  void blowFuse() { fuseState = false;}
+};
 /*
-Thing 6)
+Thing 6) Output Section
 5 properties:
     1) the number of output tubes (int)
     2) the grid bias voltage (float)
@@ -269,7 +399,26 @@ Thing 6)
     2) Admust master output volume
     3) Amplify line level audio to a speaker level signal
  */
-
+struct OutputSection
+{
+  // 1) the number of output tubes (int)
+  int numberOfOutputTubes = 2;
+  // 2) the grid bias voltage (float)
+  float gridBiasVoltage = -50.f;
+  // 3) the output transformers primary impedance in Ohms (double)
+  double outputTransformerPrimaryImpedance { 2600 };
+  // 4) the output transformers secondary impedance in Ohms (float)
+  float outputTransformerSecondaryImpedance = 8.f;
+  // 5) the maximum output power in watts (float)
+  float maxOutputPower = 50.f;
+  
+  // 1) Warm up tubes
+  void warmUpTubes(float timeInMSTillWarm);
+  // 2) Admust master output volume
+  void adjustMasterOutputVolume(float newVolume);
+  // 3) Amplify line level audio to a speaker level signal
+  float amplifyLineLevelAudioToSpeakerLevel(float inputSignal);
+};
 /*
 Thing 7) Preamp Section
 5 properties:
@@ -277,13 +426,32 @@ Thing 7) Preamp Section
     2) the gain setting for channel 1 (float)
     3) the gain setting for channel 2 (float)
     4) the active channel (int)
-    5) the grid bias resistor value for the second gain stage (int)
+    5) the cathode bias resistor value for the second gain stage (int)
 3 things it can do:
     1) warm up tubes
     2) Adjust channel gain setting
     3) Amplify guitar level signal to line voltage level
  */
-
+struct PreampSection
+{
+  // 1) the number of preamp tubes (int)
+  int numberOfPreampTubes = 2;
+  // 2) the gain setting for channel 1 (float)
+  float channelOneGain = 0.33f;
+  // 3) the gain setting for channel 2 (float)
+  float channelTwoGain = 0.33f;
+  // 4) the active channel (int)
+  int activeChannel = 0;
+  // 5) the cathode bias resistor value for the second gain stage (int)
+  int stageTwoBiasResistorValue = 820;
+  
+  // 1) warm up tubes
+  void warmUpTubes(float timeInMSTillWarm);
+  // 2) Adjust channel gain setting
+  void adjustChannelGain(int channel, float gain);
+  // 3) Amplify guitar level signal to line voltage level
+  float amplifyGuitarSignalToSpeakerLevel(float inputSignal);
+};
 /*
 Thing 8) EQ Controls
 5 properties:
@@ -297,7 +465,26 @@ Thing 8) EQ Controls
     2) Adjust Bass control knob
     3) Turn bypass on/off
  */
+struct EQControls
+{
+  // 1) The high cut (or treble) setting (float)
+  float trebleSetting = 0.5f;
+  // 2) The midrange cut setting (float)
+  float midrangeSetting = 0.5f;
+  // 3) The low frequency (or bass) cut setting (float)
+  float lowSetting = 0.5f;
+  // 4) the amount of out of phase post power amp feedback signal to mix in (presence) (float)
+  float presenceSetting { 0.5f };
+  // 5) is the EQ tone stack bypassed? (bool)
+  bool isBypassed = false;
 
+  //1) Adjust Treble control knob
+  void adjustTrebleSetting(float value);
+  //2) Adjust Bass control knob
+  void adjustBassSetting(float value);
+  //3) Turn bypass on/off
+  void setBypass(bool value);
+};
 /*
 Thing 9) Speaker Cabinet
 5 properties:
@@ -311,7 +498,26 @@ Thing 9) Speaker Cabinet
     2) adjust load attenuator
     3) release magic smoke
  */
+struct SpeakerCabinet
+{
+  // 1) the number of speakers (int)
+  int numberOfSpeakers = 1;
+  // 2) the speaker diameter (int)
+  int speakerDiameter = 12;
+  // 3) the cabinets max wattage (int)
+  int maxWattage = 70;
+  // 4) the cabinet impedance in ohms (float)
+  float impedance = 8.f;
+  // 5) the cabinets maximum reproduceable frequency (float)
+  float maxFrequency = 6000.f;
 
+  // 1) convert voltage and current to sound
+  double convertPowerToSound( float inputVoltage );
+  // 2) adjust load attenuator
+  void setAttenuatorValue( float value );
+  // 3) release magic smoke
+  void releaseMagicSmoke();
+};
 /*
 Thing 10) Tube Guitar Amp Combo
 5 properties:
@@ -325,7 +531,26 @@ Thing 10) Tube Guitar Amp Combo
     2) Adjust guitar EQ
     3) Amplify guitar sound
  */
+struct TubeGuitarAmpCombo
+{
+  // 1) Power Supply
+  PowerSupply ps;
+  // 2) Output Section
+  OutputSection outputSection;
+  // 3) Preamp Section
+  PreampSection preampSection;
+  // 4) EQ Controls (or Tone Stack)
+  EQControls eq;
+  // 5) Speaker Cabinet
+  SpeakerCabinet cabinet;
 
+  // 1) Turn on / off
+  void setPowerState( bool value );
+  // 2) Adjust guitar EQ
+  void adjustEQ( int band, float value );
+  //3) Amplify guitar sound
+  double amplifyGuitarSound( float guitarSignal );
+};
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
 
